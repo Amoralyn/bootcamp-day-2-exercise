df988394b230421ca2d88c84f6dc27f25f169fea
Lovelyn <lovelynisrael@hotmail.com>
2016-02-16 15:16:43 +0100

    Correct code in ruby file

--- string-loop.rb
+++ string-loop.rb
@@ -1,11 +1,15 @@
-counter =0
+# counter =0
 name = "ikechukwuyeremefuna"
 letters = name.upcase!.split("")
 # letters.each do |letter| 
 # 	puts "#{counter}  #{letter}" 
 # 	counter += 1
 # end
-while counter < letters.length do
-	puts "#{counter + 1} #{letters[counter]}"
-	counter += 1
+# while counter < letters.length do
+# 	puts "#{counter + 1} #{letters[counter]}"
+# 	counter += 1
+# end
+
+for i in 0..letters.length-1
+	puts "#{i + 1} #{letters[i]}"
 end
