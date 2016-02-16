9bd1db1964742add8256c34ea760bd9443d96f10
Lovelyn <lovelynisrael@hotmail.com>
2016-02-16 14:13:15 +0100

    Modify ruby file

--- string-loop.rb
+++ string-loop.rb
@@ -1,8 +1,11 @@
-counter =1
+counter =0
 name = "ikechukwuyeremefuna"
 letters = name.upcase!.split("")
-letters.each do |letter| 
-	puts "#{counter}  #{letter}" 
+# letters.each do |letter| 
+# 	puts "#{counter}  #{letter}" 
+# 	counter += 1
+# end
+while counter < letters.length - 1 do
+	puts "#{counter + 1} #{letters[counter]}"
 	counter += 1
 end
-				
