b2d83963dbe98fbd72c6174db305f02c126ba5ec
Lovelyn <lovelynisrael@hotmail.com>
2016-02-16 13:57:39 +0100

    Update ruby file

--- string-loop.rb
+++ string-loop.rb
@@ -1,3 +1,8 @@
+counter =1
 name = "ikechukwuyeremefuna"
 letters = name.upcase!.split("")
-letters.each {|letter| puts"#{letter}"}
+letters.each do |letter| 
+	puts "#{counter}  #{letter}" 
+	counter += 1
+end
+				
